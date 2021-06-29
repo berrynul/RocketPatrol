0c3bd35dd6afd9594dc1d28e0de37e9497ed9320
berrynul <blueorangerino@gmail.com>
2021-06-29 01:08:47 -0700

    fixed update probeln

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ menu, play ]
+    scene: [ menu, Play ]
 }
 let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
