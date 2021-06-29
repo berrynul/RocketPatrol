2e67eb4d71beaa43748c03ef3a328a81750e5fbb
berrynul <blueorangerino@gmail.com>
2021-06-29 00:46:28 -0700

    TESTS

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,18 +1,15 @@
-// Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
-  
-        // add object to existing scene
-        scene.add.existing(this);
-        
-        this.isFiring = false;
-        this.moveSpeed = 2;
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this); 
+        this.isFiring = false;   
+        this.moveSpeed = 2     
     }
-
+    
     update() {
+        console.log("test update")
         if(Phaser.Input.Keyboard.JustDown(keyF)){
-            console.log("shot");
+            console.log("f key pressed");
         }
         // left/right movement
         if(!this.isFiring) {
